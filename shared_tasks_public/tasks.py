3ac47ad618d09e51b74af4a1c24df8effcfd852b
Maria <mashammma@gmail.com>
2025-02-28 20:51:33 +0200

    ghj

--- shared_tasks_public/tasks.py
+++ shared_tasks_public/tasks.py
@@ -16,10 +16,7 @@ def process_spot_webhook(webhook_data):
     try:
 
         # Логирование полученных данных
-        logger.info(f"Spot webhook: {json.dumps(webhook_data, indent=2)}")
-
-        # Отправляем данные в очередь для исполнения ордера
-        execute_spot_order.apply_async(args=[webhook_data], queue='BybitSpot1', routing_key='BybitSpot1')
+        logger.info(f"Spot webhook: {json.dumps(webhook_data, indent=2)}
 
         return {"status": "processed", "data": webhook_data}
     except Exception as e:
